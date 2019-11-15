e27023a704ad351a1db00190845c9c503a39f7c3
Muhar Dwi Purwanto <k@muhar.us>
2019-11-16 03:08:19 +0700

    FIX: Tracks store

--- store/Tracks/getters.js
+++ store/Tracks/getters.js
@@ -1,1 +1,5 @@
-export default {}
+export default {
+  tracks (state) {
+    return state.tracks
+  }
+}
--- store/Tracks/mutations.js
+++ store/Tracks/mutations.js
@@ -1,1 +1,8 @@
-export default {}
+export default {
+  SET_TRACKS (state, tracks) {
+    state.tracks = tracks
+  },
+  RESET_TRACKS (state) {
+    state.tracks = []
+  }
+}
