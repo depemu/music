076b0ca8cd8be6e80af4b5b4c5eb812c35777159
Muhar Dwi Purwanto <k@muhar.us>
2019-11-20 20:57:10 +0700

    ADD: response error check on getRecentTracks action

--- store/Tracks/actions.js
+++ store/Tracks/actions.js
@@ -2,8 +2,13 @@ export default {
   getRecentTracks ({ commit }) {
     return new Promise ((resolve, reject) => {
       this.$axios.$get('https://api.lab.muhar.us/last.fm/').then((response) => {
-        commit('SET_TRACKS', response)
-        resolve(response)
+        if (!response.error) {
+          commit('SET_TRACKS', response)
+          resolve(response)
+        }
+        else {
+          reject(response)
+        }
       }).catch((error) => {
         reject(error)
       })
