289132f873cfb2f4b50e8a3d806738cd2718bcea
Muhar Dwi Purwanto <k@muhar.us>
2019-11-30 08:32:56 +0700

    CHANGE: api url

--- store/Tracks/actions.js
+++ store/Tracks/actions.js
@@ -1,7 +1,7 @@
 export default {
   getRecentTracks ({ commit }) {
     return new Promise ((resolve, reject) => {
-      this.$axios.$get('https://api.lab.muhar.us/last.fm/').then((response) => {
+      this.$axios.$get('https://api.muhar.us/last.fm/').then((response) => {
         if (!response.error) {
           commit('SET_TRACKS', response.tracks)
           resolve(response)
