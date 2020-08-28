cad85775aff19dd9f693b2253b8cb62cc1ae7fe8
Muhar Dwi Purwanto <k@muhar.us>
2020-08-28 13:02:02 +0700

    FIX: path

--- store/Tracks/actions.js
+++ store/Tracks/actions.js
@@ -1,7 +1,7 @@
 export default {
   getRecentTracks ({ commit, state }) {
     return new Promise ((resolve, reject) => {
-      this.$axios.$get(`//depemu-muhapi.herokuapp.com/last.fm`).then((response) => {
+      this.$axios.$get(`https://depemu-mapi.herokuapp.com/last.fm`).then((response) => {
         if (response.recenttracks) {
           const tracks = response.recenttracks.track
 
