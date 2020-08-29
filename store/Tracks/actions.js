2708e9aeac4713598091d35aa719a4a7638e3256
Muhar Dwi Purwanto <k@muhar.us>
2020-08-29 19:33:42 +0700

    FIX: Change api url

--- store/Tracks/actions.js
+++ store/Tracks/actions.js
@@ -1,7 +1,7 @@
 export default {
   getRecentTracks ({ commit }) {
     return new Promise ((resolve, reject) => {
-      this.$axios.$get(`//depemumapi.herokuapp.com/last.fm/`).then((response) => {
+      this.$axios.$get(`//api.muh.my.id/v1/last.fm/`).then((response) => {
         if (!response.error) {
           commit('SET_TRACKS', response.tracks)
           resolve(response)
